84d46e34b8691767410de59df0a48b8415d5ced4
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2022-02-22 18:28:51 -0800

    edit spider.py
    web docs download but path and name incorrect

--- A1/spider.py
+++ A1/spider.py
@@ -1,4 +1,6 @@
+import shutil
 import urllib.robotparser
+import urllib.request
 from urllib.request import urlopen
 from link import LinkFinder
 from domain import *
@@ -42,6 +44,7 @@ class Spider:
             if page_url not in Spider.crawled:
                 print(thread_name + ' now crawling ' + page_url)
                 print('Queue ' + str(len(Spider.queue)) + ' | Crawled  ' + str(len(Spider.crawled)))
+                Spider.download_page(page_url)
                 Spider.add_links_to_queue(Spider.gather_links(page_url))
                 Spider.queue.remove(page_url)
                 Spider.crawled.add(page_url)
@@ -80,3 +83,16 @@ class Spider:
     def update_files():
         set_to_file(Spider.queue, Spider.queue_file)
         set_to_file(Spider.crawled, Spider.crawled_file)
+
+    @staticmethod
+    def download_page(page_url):
+        file_name = Spider.clean_url(page_url)
+        urllib.request.urlretrieve(page_url, file_name)
+
+    @staticmethod
+    def clean_url(page_url):
+        result = ''
+        for char in str(page_url):
+            if char.isalnum():
+                result.join(char)
+        return result
