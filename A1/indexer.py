7bc6aaeed7a7717211b736833886552f3bd72adf
mirandamanuel <49087932+mirandamanuel@users.noreply.github.com>
2022-03-25 20:30:01 -0700

    Revert "Add files via upload"
    This reverts commit 1f77ef1e1f2748c0d09b1f3febae09ff822aabc0.

--- A1/indexer.py
+++ A1/indexer.py
@@ -1,33 +1,79 @@
-import csv
+from html.parser import HTMLParser
+from domain import *
+import collections
 import os
-import pickle
 
-class Indexer:
-    def __init__(self):
-        self.F_index = {}
-    def create_index(self, filename):
-        self.F_index = {}
-        if os.path.isfile(filename):
-            with open(filename, 'r', encoding='utf-8') as file:
-                index = 0
-                r = csv.reader(file)
-                for row in r:
-                    if row:
-                        index += 1
-                        for word in row:
-                            if word not in self.F_index:
-                                self.F_index[word] = [f"doc{index}"]
-                            else:
-                                if f"doc{index}" not in self.F_index[word]:
-                                    self.F_index[word].append(f"doc{index}")
 
-        with open('index.pkl', 'wb') as f:
-            pickle.dump(self.F_index, f)
-    def load(self, name):
-        try:
-            with open(name, 'rb') as f:
-                self.F_index = pickle.load(f)
-        except:
-            self.F_index = {}
-    def get(self):
-        return self.F_index
+class Indexer(HTMLParser):
+    # tags to search text within
+    search_tags = ['p', 'div', 'span', 'a', 'h1', 'h2', 'h2', 'h3', 'h4']
+
+    # current tag
+    current_tag = ''
+
+    # index represented as dictionary
+    index_dict = {}
+
+    file_name = ''
+
+    # handle starting tag
+    def handle_starttag(self, tag, attr):
+        # store current tag
+        self.current_tag = tag
+
+        # handle tag's data
+
+    def handle_data(self, data):
+        # make sure current tag matches search tags
+        if self.current_tag in self.search_tags:
+            # loop over word list within current tag
+            for word in data.strip().split():
+                # convert word to lowercase and filter characters
+                current_word = word.lower()
+                current_word = current_word.replace('.', '')
+                current_word = current_word.replace(':', '')
+                current_word = current_word.replace(',', '')
+                current_word = current_word.replace('"', '')
+
+                # filter words
+                if (
+                        len(current_word) >= 2 and
+                        current_word[0].isalpha()
+                ):
+                    if current_word in self.index_dict:
+                        current_list = self.index_dict[current_word]
+                        current_list.add(self.file_name)
+                    else:
+                        self.index_dict[current_word] = {self.file_name}
+
+    def get_index(self):
+        return self.index_dict
+
+    def set_file_name(self, file_name):
+        self.file_name = file_name
+
+
+
+if __name__ == '__main__':
+    PROJECT_NAME = 'repository'
+    DOMAIN_NAME = get_domain_name('https://www.cpp.edu/')
+    html_dir = os.path.join(PROJECT_NAME)
+
+
+    index = Indexer()
+    for root, dirs, files in os.walk(html_dir):
+        for file in files:
+            if file.endswith('.html'):
+                index.set_file_name(os.path.join(root, file))
+                with open(os.path.join(root, file), 'r', encoding='utf-8') as html_file:
+                    html_string = html_file.read()
+                    # feed the HTML to words parser
+                    index.feed(html_string)
+
+    with open(os.path.join(PROJECT_NAME, 'index.txt'), "a", encoding='utf-8') as f:
+        f.write(str(index.get_index()))
+
+    with open(os.path.join(PROJECT_NAME, 'index.csv'), 'w', encoding='utf-8') as f:
+        for key in index.get_index().keys():
+            print(key, str(index.get_index()[key]), sep=", ", file=f)
+
