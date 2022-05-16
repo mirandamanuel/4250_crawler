00426201fe93b6c5b65dd21b22658621059fafa7
Andy Peng <48707379+sjpmpzx@users.noreply.github.com>
2022-05-15 20:28:02 -0700

    Update page rank and link counter

--- A1/page_rank.py
+++ A1/page_rank.py
@@ -1,19 +1,17 @@
 import hashlib
-import math
+import os
 
 
-def page_rank(path, inlink_csv, iterations):
-    hash_num_dict = {}
-    # generate dictionary for {hashed_links:num_of_outlinks} using report.csv
-    with open(fr'{path}\report.csv', 'r', encoding='utf-8') as f:
-        lines = f.readlines()[1:]   # skip the first line
-        for line in lines:
-            page_url, num = line.split(', ')
-            hashed_url = hashlib.md5(page_url.encode()).hexdigest().upper()
-            hash_num_dict[hashed_url] = int(num)
+def page_rank(path, iterations):
+
+    num_outlink_dict = {}   # {hash: int}
+    with open(fr'{path}\num_of_outlink.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            page_hash, num = line.split(',')
+            num_outlink_dict[page_hash] = int(num)
 
     inlink_dict = {}    # {hash:[hash, hash...]}
-    with open(fr'{path}\{inlink_csv}', 'r', encoding='utf-8') as f:
+    with open(fr'{path}\inlink.csv', 'r', encoding='utf-8') as f:
         for line in f:
             page_hash, inlinks = line.split(',', 1)
             inlink_dict[page_hash] = inlinks.replace('\n', '').split(',')
@@ -21,70 +19,62 @@ def page_rank(path, inlink_csv, iterations):
     # start page rank
     # iteration 0
     PR = {}
-    for page in hash_num_dict:
-        PR[page] = 1 / len(hash_num_dict)
-    print(f'iteration 0: {list(PR.values())[0:5]}')
-    print(sum(list(PR.values())))
+    for page in num_outlink_dict:
+        PR[page] = 1 / len(num_outlink_dict)
+    print(f'iteration 0: {list(PR.values())[0:5]}...')
+    print(f'    Sum: {sum(list(PR.values()))}')
 
     # iteration 1~n
-    # Try x iterations for now
-    for i in range(iterations):
-    # i = 0
-    # last_result = PR.values()
-    # while True:
+    # for i in range(iterations):
+    i = 0
+    while True:
+        last_result = PR.values()
         tmp_PR = PR.copy()
-        for page in hash_num_dict:
+        for page in num_outlink_dict:
             tmp_PR[page] = 0
 
             inlinks = inlink_dict.get(page)
             if inlinks:
                 for inlink in inlinks:
-                    tmp_PR[page] += PR[inlink] / hash_num_dict[inlink]   # sum of PR(v) / N_v
+                    tmp_PR[page] += PR[inlink] / num_outlink_dict[inlink]   # sum of PR(v) / N_v
             else:   # if inlinks is None (i.e. No pages link to this page)
                 tmp_PR[page] = 0
 
         PR = tmp_PR.copy()
-        print(f'iteration {i + 1}: {list(PR.values())[0:5]}')
-        print(sum(list(PR.values())))
-        # i += 1
-        #
-        # if abs(list(PR.values())[0] - list(last_result)[0]) <= 0.000001:
-        #     break
+        print(f'iteration {i + 1}: {list(PR.values())[0:5]}...')
+        print(f'    Sum: {sum(list(PR.values()))}')
+        i += 1
+
+        # diff = abs(list(PR.values())[0] - list(last_result)[0])
+        diff = max(abs(a - b) for a, b in zip(list(PR.values()), list(last_result)))
+        print(f'    Max Diff: {diff}')
+        if diff <= 0.000001:    # converge
+            break
+
+    return dict(sorted(PR.items(), key=lambda x: x[1], reverse=True))
 
 
-# def is_not_converged():
-#     global perplexity
-#
-#     H = 0
-#     for val in PR:
-#         H += PR[val] * math.log(PR[val], 2)
-#
-#     perplexity.append(2 ** (-H))
-#
-#     if len(perplexity) < 5:
-#         return True
-#
-#     count = 0
-#     diff_elements = []
-#
-#     for i in range(len(perplexity) - 5, len(perplexity) - 1):
-#         diff_elements.append(int(perplexity[i]) - int(perplexity[i + 1]))
-#
-#     for val in diff_elements[-4:]:
-#         if val != 0:
-#             return True
-#         count += 1
-#
-#     if count == 4:
-#         return False
-#
-#     return True
+def top_100(path, PR):
+    # generate dictionary for hash:links using report.csv
+    hash_link_dict = {}
+    with open(fr'{path}\report.csv', 'r', encoding='utf-8') as f:
+        lines = f.readlines()[1:]   # skip the first line
+        for line in lines:
+            # print(line)
+            page_url, _ = line.split(', ')
+            hashed_url = hashlib.md5(page_url.encode()).hexdigest().upper()
+            hash_link_dict[hashed_url] = page_url
+
+    with open(os.path.join(path, 'top100.csv'), 'w', encoding='utf-8') as f:
+        for page in list(PR.keys())[:100]:
+            f.write(f'{hash_link_dict[page]}, {PR[page]}\n')
+
 
 
 if __name__ == '__main__':
-    path = r'repository\gmarket.co.kr'
-    inlink_csv = 'gmarket_inlink.csv'
-    iterations = 10
-    page_rank(path, inlink_csv, iterations)
+    path = r'repository\cpp.edu'
+    iterations = 20     # ignore
+    PR = page_rank(path, iterations)
+    top_100(path, PR)
 
 
